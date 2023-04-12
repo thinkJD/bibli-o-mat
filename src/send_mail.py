e15d65154d5504d6d6b373e42c6502c9ed774edd
thinkJD <jd.georgens@gmail.com>
2023-04-12 18:11:53 +0200

    add tempolate function

--- src/send_mail.py
+++ src/send_mail.py
@@ -1,5 +1,10 @@
 import requests
 
+TEMPLATE = """
+
+"""
+
+
 class SendMail():
     def __init__(self, api_token) -> None:
         self.url = "https://send.api.mailtrap.io/api/send"
@@ -7,7 +12,15 @@ class SendMail():
             "Authorization": f"Bearer {api_token}",
             "Content-Type": "application/json"
         }
-    
+
+    def render_mail():
+        payload = {'from':
+            {"email":"mailtrap@thinkjd.de",
+             "name":"Mailtrap Test"
+            },
+            "to":[{"email":"jd.georgens@gmail.com"}],"subject":"You are awesome!","text":"Congrats for sending test email with Mailtrap!","category":"Integration Test"}
+
+
     def send_mail(self):
         payload = "{\"from\":{\"email\":\"mailtrap@thinkjd.de\",\"name\":\"Mailtrap Test\"},\"to\":[{\"email\":\"jd.georgens@gmail.com\"}],\"subject\":\"You are awesome!\",\"text\":\"Congrats for sending test email with Mailtrap!\",\"category\":\"Integration Test\"}"
         response = requests.request("POST", self.url, headers=self.headers, data=payload)
